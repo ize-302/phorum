64966aa376591b4691ca4a1b6c7b19f9325631d8
ozorku <adavizeozorku@gmail.com>
2021-07-23 13:26:30 +0100

    fix error

--- models/user.model.ts
+++ models/user.model.ts
@@ -16,6 +16,6 @@ const userSchema = new Schema({
 userSchema.plugin(mongoosePaginate);
 userSchema.plugin(mongooseHidden, { hidden: { __v: true, _id: false } });
 
-const User = mongoose.models.User || mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
